7480ebf149ab2486ce266e198d52ae2b7830da68
MdHananSjd <mdhanansjd+github@gmail.com>
2025-09-23 03:22:26 +0530

    Delete frontend directory
    Too many fuck ups

--- frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-black/70 text-gray-400 py-4 text-center z-20">
-      <p>© {new Date().getFullYear()} GridShield AI. All rights reserved.</p>
-    </footer>
-  );
-}
